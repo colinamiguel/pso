a102c09987c049544bad605f6d5310fbb95b04c7
colinamiguel <miguelcolina@correo.unimet.edu.ve>
2022-05-30 19:23:49 -0400

    Miguel

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Interface main_interface = new Interface();
+        main_interface.setVisible(true);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
